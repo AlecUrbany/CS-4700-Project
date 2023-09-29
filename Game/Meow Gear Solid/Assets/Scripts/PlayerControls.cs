9f5c2c0d6374dfc4a25f8cb6b5294027340834f8
Vincent Rivera <rivera.vincent2002@yahoo.com>
2023-09-28 17:18:56 -0700

    New Controls

--- Game/Meow Gear Solid/Assets/Scripts/PlayerControls.cs
+++ Game/Meow Gear Solid/Assets/Scripts/PlayerControls.cs
@@ -6,29 +6,53 @@ public class PlayerControls : MonoBehaviour
 {
 
 //All the commented out code involves moving where the character looks. Might implement back in
-	public float moveSpeed = 8;
-	public float rotationSpeed = 720;
+	//public float moveSpeed = 8;
+	//public float rotationSpeed = 720;
 
-	Rigidbody rigidbody;
+	//Rigidbody rigidbody;
 
-	Vector3 velocity;
+	//Vector3 velocity;
+
+	[SerializeField] private float _moveSpeed = 3f;
+
+	[SerializeField] private float _turnSpeed = 30f;
+
+	private CharacterController _cc;
 
 
 	void Start ()
     {
-		rigidbody = GetComponent<Rigidbody> ();
+		//rigidbody = GetComponent<Rigidbody> ();
 		//viewCamera = Camera.main;
+
+		_cc = GetComponent<CharacterController>();
 	}
 
 	void Update ()
     {
-		float horizInput = Input.GetAxisRaw ("Horizontal");
-		float vertInput = Input.GetAxisRaw ("Vertical");
-		velocity = new Vector3 (horizInput, 0, vertInput).normalized * moveSpeed;
+		//float horizInput = Input.GetAxisRaw ("Horizontal");
+		//float vertInput = Input.GetAxisRaw ("Vertical");
+		//velocity = new Vector3 (horizInput, 0, vertInput).normalized * moveSpeed;
+		if (Input.GetKey(KeyCode.W))
+        {
+            _cc.Move(transform.forward * _moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _cc.Move(-transform.forward * _moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -_turnSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, _turnSpeed * Time.deltaTime, 0);
+        }
 	}
 
 	void FixedUpdate()
     {
-		rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
+		//rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
 	}
 }
