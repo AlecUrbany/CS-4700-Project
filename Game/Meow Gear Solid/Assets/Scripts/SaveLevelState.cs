bad341c0779e26fbd4f02d0a120d9064b4757f67
Jose Pavon <RannerJP@gmail.com>
2023-10-26 01:27:09 -0700

    WIP - Saves the current level state

---
+++ Game/Meow Gear Solid/Assets/Scripts/SaveLevelState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLevelState : MonoBehaviour{
+    public string levelToSave;
+    public void Start(){
+    LevelState state = new LevelState();
+    state.enemyPositions = GetEnemyPositions();
+    state.collectedItems = GetDrops();
+
+    string jsonState = JsonUtility.ToJson(state);
+    PlayerPrefs.SetString(levelToSave, jsonState);
+    PlayerPrefs.Save();
+    }
+
+    public void GetEnemyPositions(){
+        List<Enemy> enemyPositions = new List<Vector3>();
+        foreach (Enemy enemy in enemiesInLevel)
+        {
+            enemyPositions.Add(enemy.transform.position);
+        }
+        return enemyPositions;
+    }
+
+}
