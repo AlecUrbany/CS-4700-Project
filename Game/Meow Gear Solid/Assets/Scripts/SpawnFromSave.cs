6d0094b4f1f399c3a5fdf92e09e7ee825454b35e
Jose Pavon <RannerJP@gmail.com>
2023-11-23 07:32:14 -0800

    spawns player, enemies, drops

---
+++ Game/Meow Gear Solid/Assets/Scripts/SpawnFromSave.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+public class GameData{
+    public EnemyDataLoad[] enemies;
+    public DropData[] drops;
+}
+
+[System.Serializable]
+public class EnemyDataLoad{
+    public Vector3 position;
+    public float health;
+    public int aiState;
+    public Vector3 startPosition;
+    public float moveSpeed;
+    public float patrolDistance;
+    public float viewRadius;
+}
+
+[System.Serializable]
+public class DropData{
+    public Vector3 position;
+    public string itemName;
+}
+
+public class SpawnFromSave : MonoBehaviour{
+    public string defaultSpawnPointName = "DefaultSpawn"; //Default location
+    public Transform player;
+    public GameObject enemyPrefab;
+
+    private void Start(){
+        if(GameObject.FindGameObjectsWithTag("Player").Length > 1){
+            Destroy(gameObject); //Check if the player from the last scene is in this one and destories
+        }
+        string sceneName = SceneManager.GetActiveScene().name;
+        string folderPath = Path.Combine(Application.dataPath, "../LevelStates");
+        string fileName = sceneName + ".json";
+        string filePath = Path.Combine(folderPath, fileName);
+        if(File.Exists(filePath)){
+            Debug.Log("File exists");
+            DestroyGameObjectsWithTag("Enemy");
+            //DestroyGameObjectsWithTag("Drop");
+            string jsonText = System.IO.File.ReadAllText(filePath);
+            GameData gameData = JsonUtility.FromJson<GameData>(jsonText);
+            foreach (EnemyDataLoad enemyData in gameData.enemies){
+                GameObject enemy = Instantiate(enemyPrefab, enemyData.position, Quaternion.identity);
+                EnemyAI enemyAI = enemy.GetComponent<EnemyAI>();
+                enemyAI.player = player;
+                enemyAI.chaseStartPosition = enemyData.startPosition;
+                enemyAI.aiType = (EnemyAIType)enemyData.aiState;
+                enemyAI.patrolDistance = enemyData.patrolDistance;
+                enemyAI.moveSpeed = enemyData.moveSpeed;
+                visionCone cone = enemy.GetComponentInChildren<visionCone>();
+                cone.viewRadius = enemyData.viewRadius;
+                EnemyHealth health = enemy.GetComponent<EnemyHealth>();
+                health.currentHealth = enemyData.health;
+            }
+
+        }
+        string targetSpawnPointName = PlayerPrefs.GetString("TargetSpawnPoint", defaultSpawnPointName);
+        Transform targetSpawnPoint = GameObject.Find(targetSpawnPointName).transform;
+        player.position = targetSpawnPoint.position - new Vector3(3, 0.75f, 0);
+    }
+
+    void DestroyGameObjectsWithTag(string tag){
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(tag);
+
+        foreach (GameObject gameObject in gameObjects){
+            Destroy(gameObject);
+        }
+    }
+}
